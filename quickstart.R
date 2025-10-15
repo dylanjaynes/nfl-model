2a21487186cc3cce994cd2775229d29944af21ea
dylanjaynes <83788250+dylanjaynes@users.noreply.github.com>
2025-10-14 23:05:58 -0500

    Update quickstart.R

--- quickstart.R
+++ quickstart.R
@@ -1,7 +1,8 @@
 #quickstart.R
 set.seed(42)
 source("R/utils.R"); source("R/run_week.R"); source("R/week_runner.R")
-source("R/best_bet.R"); source("R/evaluate_week.R")
+source("R/best_bet.R"); source("R/evaluate_week.R"); source("R/blend.R");
+source("R/calibration.R"); source("R/metrics.R"); source("R/projectors.R");
 
 targets <- learn_target_sds(2018:2024)
 wk <- run_weekly_report(season = 2025, week = 5, targets = targets, sims = 2000, bankroll = 500)
