81f93b2bee5c54c0933bc06574eb4ac9d6cbfcfa
dylanjaynes <83788250+dylanjaynes@users.noreply.github.com>
2025-10-14 23:04:43 -0500

    Create quickstart.R

---
+++ quickstart.R
@@ -0,0 +1,10 @@
+#quickstart.R
+set.seed(42)
+source("R/utils.R"); source("R/run_week.R"); source("R/week_runner.R")
+source("R/best_bet.R"); source("R/evaluate_week.R")
+
+targets <- learn_target_sds(2018:2024)
+wk <- run_weekly_report(season = 2025, week = 5, targets = targets, sims = 2000, bankroll = 500)
+print(head(wk$card, 10))
+ev <- evaluate_week(wk$all_games, season_target = 2025, week_target = 5)
+print(ev$summary)
